5f30c31f8d38a40c03bf2c5861d151b2a4633004
Shubhankar2650 <kshubhankar2000@gmail.com>
2024-01-18 15:24:43 +0530

    Day 8

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ export const Navbar = () => {
                         <li><NavLink to="/about">About</NavLink></li>
                         <li><NavLink to="/services">Services</NavLink></li>
                         <li><NavLink to="/contact">Contact</NavLink></li>
-                        <li><NavLink to="/register">Contact</NavLink></li>
+                        <li><NavLink to="/register">Register</NavLink></li>
                         <li><NavLink to="/login">Login</NavLink></li>
                     </ul>
                 </nav>
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,3 +1,44 @@
 export const Home = () => {
-    return <h1>Welcome to home page</h1>;
+    return <>
+    <main>
+        <section className="section-home">
+            <div className="grid grid-two-cols">
+                <div className="home-content">
+                    <p>We are no. 1</p>
+                    <h1>Welcome to the SK tutorails</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit consequatur inventore
+                         quidem qui cumque. Impedit cum nesciunt aspernatur corporis. Necessitatibus, corporis 
+                         labore? Quis fugiat ducimus sint sapiente quod fuga, cumque ad ex iste cum! Labore laudantium
+                        saepe dicta recusandae eligendi facilis unde deleniti. Molestiae illum quisquam culpa nihil eos voluptate.
+                    </p>
+                    <div className="btn btn-group">
+                        <a href="/contact"><button className="btn">Contant Now</button></a>
+                        <a href="/services"><button className="btn">Learn More</button></a>
+                    </div>
+                </div>
+                <div className="home-image">
+                    <img src="/images/laptop.png" alt="welcome" srcset="" />
+                </div>
+            </div>
+        </section>
+
+        <section className="section-activity">
+            <div className="grid grid-four-cols">
+                <div className="activity-box">
+                    <span>A</span>
+                </div>
+                <div className="activity-box">
+                    <span>B</span>
+                </div>
+                <div className="activity-box">
+                    <span>C</span>
+                </div>
+                <div className="activity-box">
+                    <span>D</span>
+                </div>
+            </div>
+        </section>
+    </main>
+    </>;
 };
